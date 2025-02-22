17452a018f61388681707ac8f53cc17fc89e5060
KarGan3 <chganesh1206@gmail.com>
2025-02-22 14:35:10 +0530

    push

--- A_Theatre_Square.cpp
+++ A_Theatre_Square.cpp
@@ -1,10 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int m,n,a;
+    long long m,n,a;
     cin>>m>>n>>a;
-    int b = (n+a-1)/a;
-    int c = (m+a-1)/a;
-    int d = b*c;
-    cout<<d;
+    if(a == 1 && m == n){
+        long long c = m*n;
+        cout<<c;
+    }
+    else{
+        long long b = (n+a-1)/a;
+        long long c = (m+a-1)/a;
+        long long d = b*c;
+        cout<<d;
+    }
+    return 0;
 }
